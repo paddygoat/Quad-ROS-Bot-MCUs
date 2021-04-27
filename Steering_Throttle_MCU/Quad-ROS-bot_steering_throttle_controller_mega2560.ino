0ba835fe982b8e06d8225e8519b52f06e48fc4fd
TegwynTwmffat <paddy@goatindustries.co.uk>
2021-04-27 19:19:40 +0100

    Update Quad-ROS-bot_steering_throttle_controller_mega2560.ino

--- Steering_Throttle_MCU/Quad-ROS-bot_steering_throttle_controller_mega2560.ino
+++ Steering_Throttle_MCU/Quad-ROS-bot_steering_throttle_controller_mega2560.ino
@@ -7,6 +7,7 @@ std_msgs::Float32 throttle_msg;
 std_msgs::Float32 ArduinoGPSx_msg;
 std_msgs::Float32 ArduinoGPSy_msg;
 std_msgs::Float32 act_steer_ang_msg;
+std_msgs::Float32 throttControl_msg;
 
 ros::Publisher pub_throttle("throttle", &throttle_msg);
 ros::Publisher pub_ArduinoGPSx("ArduinoGPSx", &ArduinoGPSx_msg);
@@ -22,36 +23,64 @@ const int ledPin1 =  13;
 const int ledPin0 =  12;
 int ledState = LOW;
 unsigned long prevMillis_01 = 0;
+unsigned long prevMillis_02 = 0;
+unsigned long prevMillis_03 = 0;
 const long interval_01 = 50;
+const long interval_02 = 250;
+const long interval_03 = 250;
 
-int sensorPin_4 = A4;
-int sensorPin_1 = A1;
-int sensorPin_2 = A2;
-int sensorPin_3 = A3;
-int sensorPin_5 = A5;    // Actual steering angle.
+int ledState_02 = LOW;
+int ledState_03 = LOW;
 
-int sensorValue_4 = 0;
-int sensorValue_1 = 0;
-int sensorValue_2 = 0;
-int sensorValue_3 = 0;
-int sensorValue_5 = 0;
+int sensorPin_1 = A1;  // Steering control
+int sensorPin_2 = A2;  // Throttle
+int sensorPin_3 = A3;  // Not used
+int sensorPin_4 = A4;  // Not used
+int sensorPin_5 = A5;  // Actual mechanical steering angle
+
+int steerControlVal = 0;  // Steering control
+int throttleVal = 0;  // Throttle
+int sensorValue_3 = 0;  // Not used
+int sensorValue_4 = 0;  // Not used
+int sensorValue_5 = 0;  // Actual mechanical steering angle
 
 int speed = 0;
 
-void messageCb( const std_msgs::Float32 dogx1_msg , const std_msgs::Float32 dogy1_msg)
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Subscriber call back setup:
+void messageCb1( const std_msgs::Float32 dogx1_msg )
 {
-  digitalWrite(12, HIGH-digitalRead(12));   // blink the led 12
   ArduinoGPSx_msg = dogx1_msg;
-  ArduinoGPSy_msg = dogy1_msg;
+  // blinkLED_12();
 }
-ros::Subscriber<std_msgs::Float32> sub0("dogx1_msg", &messageCb );
-ros::Subscriber<std_msgs::Float32> sub1("dogy1_msg", &messageCb );
+ros::Subscriber<std_msgs::Float32> sub1("dogx1_msg", &messageCb1 );
+
+void messageCb2( const std_msgs::Float32 dogy1_msg )
+{
+  ArduinoGPSy_msg = dogy1_msg;
+  // blinkLED_13();
+}
+ros::Subscriber<std_msgs::Float32> sub2("dogy1_msg", &messageCb2 );
+
+void messageCb3( const std_msgs::Float32 throttControl_msg )
+{
+  blinkLED_12();
+  speed = (int)throttControl_msg.data;
+}
+ros::Subscriber<std_msgs::Float32> sub3("throttControl_msg", &messageCb3 );
+
+////////////////////////////////////////////////////////////////////////////////////////////
 
 void setup()
 {
   nh.initNode();
-  nh.subscribe(sub0);
+
+//////////////////////////////////////////////
   nh.subscribe(sub1);
+  nh.subscribe(sub2);
+  nh.subscribe(sub3);
+/////////////////////////////////////////////
+
   nh.advertise(pub_throttle);
   nh.advertise(pub_ArduinoGPSx);
   nh.advertise(pub_ArduinoGPSy);
@@ -62,12 +91,11 @@ void setup()
   pinMode(ledPin1, OUTPUT);
   pinMode(11, OUTPUT);        // THROTTLE PWM
 
-  digitalWrite(ledPin0, HIGH);
-  digitalWrite(ledPin1, HIGH);
+  digitalWrite(ledPin0, HIGH);  // LED 12
+  digitalWrite(ledPin1, HIGH);  // LED 13
   delay(1000);
   digitalWrite(ledPin0, LOW);
   digitalWrite(ledPin1, LOW);
-  
 }
 
 void loop() 
@@ -79,65 +107,57 @@ void loop()
   {
     prevMillis_01 = currMillis;
 
+    steerControlVal = analogRead(sensorPin_1);  // Steering control
+    throttleVal = analogRead(sensorPin_2);  // Throttle
+    sensorValue_3 = analogRead(sensorPin_3);  // Not used
+    sensorValue_4 = analogRead(sensorPin_4);  // Not used
+    sensorValue_5 = analogRead(sensorPin_5);  // Actual steering angle. 575 to 408 to 276. Centre is 408.
 
-    sensorValue_1 = analogRead(sensorPin_1);
-    sensorValue_2 = analogRead(sensorPin_2);
-    sensorValue_3 = analogRead(sensorPin_3);
-    sensorValue_4 = analogRead(sensorPin_4);
-    sensorValue_5 = analogRead(sensorPin_5);   // Actual steering angle. 575 to 408 to 276. Centre is 408.
-
- 
-    // Serial.print("sensorValue_1: ");Serial.println(sensorValue_1);
-    // Serial.print("sensorValue_2: ");Serial.println(sensorValue_2);
+    // Serial.print("steerControlVal: ");Serial.println(steerControlVal);
+    // Serial.print("throttleVal: ");Serial.println(throttleVal);
     // Serial.print("sensorValue_3: ");Serial.println(sensorValue_3);
     // Serial.print("sensorValue_4: ");Serial.println(sensorValue_4);
     // Serial.println("");
 
-    // STEERING IS ON ANALOG 1:
-    if ((sensorValue_1 > 300)&&(sensorValue_1 < 750))
+    // STEERING CONTROL IS ON ANALOG 1:
+    if ((steerControlVal > 300)&&(steerControlVal < 750)) // Deadspot between 300 and 750.
     {
       digitalWrite(49, LOW);
       delay(50);
       digitalWrite(51, LOW);    // Stop the actuator
     }
 
-    if (sensorValue_1 > 750)
+    if (steerControlVal > 750)
     {
       digitalWrite(49, HIGH);    // Turn right
       delay(50);
       digitalWrite(51, HIGH);    // Move the actuator
     }
 
-    if (sensorValue_1 < 300)
+    if (steerControlVal < 300)
     {
       digitalWrite(49, LOW);     // Turn left
       delay(50);
       digitalWrite(51, HIGH);    // Move the actuator
     }
 
-    // THROTTLE IS ON ANALOG 2:
-
-    speed = (450 - (sensorValue_2)) /4;
-    if (speed < 0)
+    // THROTTLE IS ON ANALOG 2. Mid point on joystick is about = 488.
+    if(throttleVal < 480)
     {
-      speed = 0;
+      blinkLED_13();  // Blink LED 13 at 2Hz.
+      speed = (450 - throttleVal) /3.5;
+      if (speed < 0)
+      {
+        speed = 0;
+      }
     }
-    
+
     analogWrite(11, speed);
     // Serial.print("speed: ");Serial.println(speed);
 
-    
-    //if (ledState == LOW) 
-    //{
-    //  ledState = HIGH;
-    //} else 
-    //{
-    //  ledState = LOW;
-    //}
-    // digitalWrite(ledPin1, ledState);
-
     throttle_msg.data = speed;
-    act_steer_ang_msg.data = sensorValue_5;
+    act_steer_ang_msg.data = sensorValue_5;  // Actual mechanical steering angle
+
     pub_throttle.publish(&throttle_msg);
     pub_ArduinoGPSx.publish(&ArduinoGPSx_msg);
     pub_ArduinoGPSy.publish(&ArduinoGPSy_msg);
@@ -145,4 +165,34 @@ void loop()
   }
   delay(1);
   nh.spinOnce();
+}
+
+void blinkLED_13() 
+{
+  unsigned long currentMillis = millis();
+  if (currentMillis - prevMillis_03 >= interval_03) 
+  {
+    prevMillis_03 = currentMillis;
+    if (ledState_03 == LOW) {
+      ledState_03 = HIGH;
+    } else {
+      ledState_03 = LOW;
+    }
+    digitalWrite(ledPin1, ledState_03);
+  }
+}
+
+void blinkLED_12() 
+{
+  unsigned long currentMillis = millis();
+  if (currentMillis - prevMillis_02 >= interval_02) 
+  {
+    prevMillis_02 = currentMillis;
+    if (ledState_02 == LOW) {
+      ledState_02 = HIGH;
+    } else {
+      ledState_02 = LOW;
+    }
+    digitalWrite(ledPin0, ledState_02);
+  }
 }
